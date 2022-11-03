9dec931e8b76b8c2164d2bf4398e5a49da072389
Joseph Wambugu <joseeyatwork@gmail.com>
2022-11-03 16:01:51 +0300

    Updated documentation swagger

--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -14,21 +14,46 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-from rest_framework.schemas import get_schema_view
+# from rest_framework.schemas import get_schema_view
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="BookStore Inventory Management System api",
+      terms_of_service="",
+      contact=openapi.Contact(email=""),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('inventory.api.urls', namespace='api')),
     path('api/auth/', include('auth.api.urls', namespace='auth_api')),
-    path('swagger-ui/', TemplateView.as_view(
-        template_name='swagger-ui.html',
-        extra_context={'schema_url':'openapi-schema'}
-    ), name='swagger-ui'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-     path('openapi', get_schema_view(
-            title="BookStore Inventory Management System api",
-            description="",
-            version="1.0.0"
-        ), name='openapi-schema'),
+
+    
+    # path('swagger-ui/', TemplateView.as_view(
+    #     template_name='swagger-ui.html',
+    #     extra_context={'schema_url':'openapi-schema'}
+    # ), name='swagger-ui'),
+
+    #  path('openapi', get_schema_view(
+    #         title="BookStore Inventory Management System api",
+    #         description="",
+    #         version="1.0.0"
+    #     ), name='openapi-schema'),
 ]
